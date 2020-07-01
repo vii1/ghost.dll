c61b7465d1de986ff06b335c8d418b1f68908305
vii <vii@divero.net>
2020-07-01 23:10:23 +0200

    First version

---
+++ ghost.cpp
@@ -0,0 +1,61 @@
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+
+#define GLOBALS
+#include "div.h"
+
+typedef unsigned char byte;
+
+// Devuelve el color de la paleta m s pr¢ximo a los valores RGB dados
+// OJO, probablemente puede optimizarse
+byte find_color(int r, int g, int b)
+{
+  // Cada color es un punto en el cubo de color (0..63,0..63,0..63)
+  // Tenemos un punto arbitrario (rgb) y queremos encontrar el color m s cercano.
+  // La distancia es sqrt(dr*dr+dg*dg+db*db).
+  // Podemos prescindir de la ra¡z cuadrada ya que s¢lo queremos comparar distancias.
+  byte* pal = (byte*)_palette;
+  int dist = 64*64*3;
+  byte col = 0, c = 0;
+  for(byte* p = pal; p < pal+(256*3); p+=3, ++c) {
+    int dr = r-int(p[0]);
+    int dg = g-int(p[1]);
+    int db = b-int(p[2]);
+    int d = dr*dr+dg*dg+db*db;
+    if(d < dist) {
+      dist = d;
+      col = c;
+      if(!d) break;
+    }
+  }
+  return col;
+}
+
+// Sustituye la tabla ghost de DIV por una calculada por nosotros,
+// que suma los componentes RGB en vez de promediarlos
+void make_ghost_additive()
+{
+  byte* gho = (byte*)ghost;
+  byte* pal1 = (byte*)_palette;
+  for(int i = 0; i < 256; ++i, pal1+=3) {
+    gho[i+256*i] = find_color(int(pal1[0])*2,int(pal1[1])*2,int(pal1[2])*2);
+    byte* pal2 = (byte*)_palette;
+    for(int j = 0; j < i; ++j, pal2+=3) {
+      byte c = find_color(int(pal1[0])+pal2[0],int(pal1[1])+pal2[1],int(pal1[2])+pal2[2]);
+      gho[i+256*j] = c;
+      gho[j+256*i] = c;
+    }
+  }
+}
+
+// Refrescamos nuestra tabla ghost personalizada cada vez que
+// cargamos una nueva paleta
+void process_palette(void) {
+  make_ghost_additive();  
+}
+
+void __export divmain(COMMON_PARAMS) {
+  GLOBAL_IMPORT();
+  DIV_export("process_palette", process_palette);
+}
---
+++ makefile
@@ -0,0 +1,13 @@
+all: ghost.dll .SYMBOLIC
+
+ghost.dll: ghost.cpp tab_cuad.h div.h
+	wcl386 ghost.cpp -ox -s -l=div_dll
+
+.SILENT
+clean: .SYMBOLIC
+	-del ghost.err
+	-del ghost.dll
+	-del ghost.obj
+
+install: ghost.dll .SYMBOLIC
+	copy /y ghost.dll c:\div2\dll
